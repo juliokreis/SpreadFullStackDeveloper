0a39477ff1ca843fe78db11f9412d4a40bdbc1f5
juliokreis <juliokreis@hotmail.com>
2022-04-22 14:37:35 -0300

    add conteúdo as pastas

---
+++ HTML e CSS3/Primeiros passos para desenvolvimento web/site teste PHP/index.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+	<title>Meu primeiro sistema PHP! Yeeeesss!!!</title>
+</head>
+
+<body>
+	<?php
+		for($i = 0; $i < 10; $i++){
+			print("Linha número" . $i . "<br />");
+		}
+	?>
+
+</body>
+
+</html>
